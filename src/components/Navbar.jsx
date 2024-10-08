3b4e4ba4f098ebb81fed8e9cd0726c47a5efc915
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-10-08 18:07:13 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p className="text-2xl text-blue-600 font-semibold font-serif text-center py-8">NavBar</p>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Button from "../components/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Button />
+    </div>
+  );
+};
+
+export default Home;
