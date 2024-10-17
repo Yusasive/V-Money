3d47796f0cc71e30e73c66f70dcf60e957b39d31
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-10-17 22:40:34 +0100

    src/pages/Footer.jsx

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FAQs from "../components/FAQs";
+import PreFooter from "../components/PreFooter";
+import FooterConst from "../components/Footer";
+
+const Footer = () => {
+  return (
+    <div>
+      <FAQs />
+      <PreFooter />
+      <FooterConst />
+    </div>
+  );
+};
+
+export default Footer;
